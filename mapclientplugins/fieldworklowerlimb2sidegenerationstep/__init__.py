da2f2f04932ae7bd4db0a401a36a76c595f538d0
Ju Zhang <jzha263@aucklanduni.ac.nz>
2017-11-01 07:42:25 +1300

    fixed location version

--- mapclientplugins/fieldworklowerlimb2sidegenerationstep/__init__.py
+++ mapclientplugins/fieldworklowerlimb2sidegenerationstep/__init__.py
@@ -5,7 +5,7 @@ MAP Client Plugin
 __version__ = '1.0.3'
 __author__ = 'Ju Zhang'
 __stepname__ = 'Fieldwork Lower Limb 2 Side Generation'
-__location__ = 'https://github.com/mapclient-plugins/fieldworklowerlimb2sidegenerationstep/archive/v1.0.1.zip'
+__location__ = 'https://github.com/mapclient-plugins/fieldworklowerlimb2sidegenerationstep/archive/v1.0.3.zip'
 
 
 # import class that derives itself from the step mountpoint.
