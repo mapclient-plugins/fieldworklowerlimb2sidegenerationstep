de9c7c56c97958137d6b9110f4e92126f311fe1b
TimmyNic <tim.nicolas@outlook.com>
2021-06-22 17:59:50 +1200

    Fixed LowerLimbGenerationDialog

--- mapclientplugins/fieldworklowerlimb2sidegenerationstep/step.py
+++ mapclientplugins/fieldworklowerlimb2sidegenerationstep/step.py
@@ -1,6 +1,6 @@
-'''
+"""
 MAP Client Plugin Step
-'''
+"""
 import json
 
 from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
@@ -19,7 +19,7 @@ DEFAULT_MODEL_LANDMARKS = (
 
 
 class FieldworkLowerLimb2SideGenerationStep(WorkflowStepMountPoint):
-    '''
+    """
     Step for customising the lower limb bones to motion capture markers.
 
     Inputs
@@ -36,7 +36,7 @@ class FieldworkLowerLimb2SideGenerationStep(WorkflowStepMountPoint):
         "tibiafibula-r", "tibia-l", "tibia-r", "fibula-l", 'fibula-r",
         "patella-l", "patella-r".
     LowerLimbAtlas : LowerLimbAtlas instance
-    '''
+    """
 
     def __init__(self, location):
         super(FieldworkLowerLimb2SideGenerationStep, self).__init__('Fieldwork Lower Limb (2 sides) Generation',
@@ -71,39 +71,38 @@ class FieldworkLowerLimb2SideGenerationStep(WorkflowStepMountPoint):
         self._data = llstep.LLStepData(self._config)
 
     def execute(self):
-        '''
+        """
         Add your code here that will kick off the execution of the step.
         Make sure you call the _doneExecution() method when finished.  This method
         may be connected up to a button in a widget for example.
-        '''
+        """
         # Put your execute step code here before calling the '_doneExecution' method.
         self._data.loadData()
         self._data.updateFromConfig()
         print('LL estimation configs:')
-        print((self._data.config))
+        print(self._data.config)
         if self._config['GUI'] == 'True':
-            # start gui
-            self._widget = LowerLimbGenerationDialog(self._data, self._doneExecution)
-            self._widget.setModal(True)
-            self._setCurrentWidget(self._widget)
+            # Start gui
+            widget = LowerLimbGenerationDialog(self._data, self._doneExecution)
+            self._setCurrentWidget(widget)
         else:
             self._data.register()
             self._doneExecution()
 
     def setPortData(self, index, dataIn):
-        '''
+        """
         Add your code here that will set the appropriate objects for this step.
         The index is the index of the port in the port list.  If there is only one
         uses port for this step then the index can be ignored.
-        '''
+        """
         self._data.inputLandmarks = dataIn  # http://physiomeproject.org/workflow/1.0/rdf-schema#landmarks
 
     def getPortData(self, index):
-        '''
+        """
         Add your code here that will return the appropriate objects for this step.
         The index is the index of the port in the port list.  If there is only one
         provides port for this step then the index can be ignored.
-        '''
+        """
         if index == 1:
             print(('outputting {}'.format(list(self._data.outputModelDict.keys()))))
             return self._data.outputModelDict
@@ -111,13 +110,13 @@ class FieldworkLowerLimb2SideGenerationStep(WorkflowStepMountPoint):
             return self._data.LL
 
     def configure(self):
-        '''
+        """
         This function will be called when the configure icon on the step is
         clicked.  It is appropriate to display a configuration dialog at this
         time.  If the conditions for the configuration of this step are complete
         then set:
             self._configured = True
-        '''
+        """
         dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
@@ -132,29 +131,29 @@ class FieldworkLowerLimb2SideGenerationStep(WorkflowStepMountPoint):
         self._configuredObserver()
 
     def getIdentifier(self):
-        '''
+        """
         The identifier is a string that must be unique within a workflow.
-        '''
+        """
         return self._config['identifier']
 
     def setIdentifier(self, identifier):
-        '''
+        """
         The framework will set the identifier for this step when it is loaded.
-        '''
+        """
         self._config['identifier'] = identifier
 
     def serialize(self):
-        '''
+        """
         Add code to serialize this step to disk. Returns a json string for
         mapclient to serialise.
-        '''
+        """
         return json.dumps(self._config, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     def deserialize(self, string):
-        '''
+        """
         Add code to deserialize this step from disk. Parses a json string
         given by mapclient
-        '''
+        """
         self._config.update(json.loads(string))
 
         d = ConfigureDialog()
