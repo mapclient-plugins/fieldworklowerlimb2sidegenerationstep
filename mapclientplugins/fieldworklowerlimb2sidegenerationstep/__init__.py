65e0008a6a932264f23b7affb2b8ad5181f74ec9
TimmyNic <tim.nicolas@outlook.com>
2022-12-15 16:11:33 +1300

    Set version to 1.0.4

--- mapclientplugins/fieldworklowerlimb2sidegenerationstep/__init__.py
+++ mapclientplugins/fieldworklowerlimb2sidegenerationstep/__init__.py
@@ -1,7 +1,7 @@
 """
 MAP Client Plugin
 """
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __author__ = 'Ju Zhang'
 __stepname__ = 'Fieldwork Lower Limb 2 Side Generation'
 __location__ = 'https://github.com/mapclient-plugins/fieldworklowerlimb2sidegenerationstep/archive/v1.0.3.zip'
